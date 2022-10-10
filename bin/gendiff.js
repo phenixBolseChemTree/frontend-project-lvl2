15763b00cc9df05d7cbcbb028ca26a89bb99c3f1
ivan sharapov <ivansharapov@ivans-MacBook-Pro.local>
2022-10-10 17:09:02 +0300

    packege.json lock.json bin/gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,10 +1,22 @@
 #!/usr/bin/env node
 import { program } from 'commander';
 
-console.log();
-console.log('Compares two configuration files and shows a difference.')
 console.log('');
+
 program
   .version('0.0.1')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f --format','<type> output format')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .action((str, options) => {
+    console.log();
+  })
+//полезные функции: path.resolve() и process.cwd()).
+
+
+.action((strings, options) => {
+  console.log(strings.join(options.separator));
+});
 
 program.parse();
