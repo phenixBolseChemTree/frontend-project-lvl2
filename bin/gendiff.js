dba04f4a316bcb8478bae94d5ec603e7b5d4b9bc
ivan sharapov <ivansharapov@ivans-MacBook-Pro.local>
2022-10-12 18:51:49 +0300

    install eslint Makefile and refactor code eslint

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,14 +1,13 @@
 #!/usr/bin/env node
-import { program } from "commander";
-import { diff } from "../index.js";
-console.log("");
+import { program } from 'commander';
+import diff from '../index.js';
 
 program
-  .version("0.0.1")
-  .description("Compares two configuration files and shows a difference.")
-  .option("-f --format", "<type> output format")
-  .argument("<filepath1>")
-  .argument("<filepath2>")
+  .version('0.0.1')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f --format', '<type> output format')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
   .action((file1, file2) => {
     console.log(diff(file1, file2));
   });
--- index.js
+++ index.js
@@ -1,59 +1,58 @@
-import _ from "lodash";
-import * as fs from "node:fs";
+// eslint-disable-next-line no-unused-vars
+import _ from 'lodash';
+import * as fs from 'node:fs';
 
-export const diff = (fileName1, fileName2) => {
-  const file1 = JSON.parse(fs.readFileSync(fileName1, "utf-8"));
-  const file2 = JSON.parse(fs.readFileSync(fileName2, "utf-8"));
+const diff = (fileName1, fileName2) => {
+  const file1 = JSON.parse(fs.readFileSync(fileName1, 'utf-8'));
+  const file2 = JSON.parse(fs.readFileSync(fileName2, 'utf-8'));
   const keysNotFiltred = Object.keys(file1).concat(Object.keys(file2)).sort();
 
-  const keys = keysNotFiltred.filter((element, index) => {
-    return keysNotFiltred.indexOf(element) === index;
-  });
-  
+  const keys = keysNotFiltred.filter((element, index) => keysNotFiltred.indexOf(element) === index);
+
   const resultArr = [];
   keys.forEach((key) => {
     if (file1.hasOwnProperty(key) && file2.hasOwnProperty(key)) {
       if (file1[key] !== file2[key]) {
         resultArr.push({
-          key: key,
-          type: "-",
+          key,
+          type: '-',
           value: file1[key],
         });
         resultArr.push({
-          key: key,
-          type: "+",
+          key,
+          type: '+',
           value: file2[key],
         });
       } else {
         resultArr.push({
-          key: key,
-          type: " ",
+          key,
+          type: ' ',
           value: file1[key],
         });
       }
+    } else if (file1.hasOwnProperty(key)) {
+      resultArr.push({
+        key,
+        type: '-',
+        value: file1[key],
+      });
     } else {
-      if (file1.hasOwnProperty(key)) {
-        resultArr.push({
-          key: key,
-          type: "-",
-          value: file1[key],
-        });
-      } else {
-        resultArr.push({
-          key: key,
-          type: "+",
-          value: file2[key],
-        });
-      }
+      resultArr.push({
+        key,
+        type: '+',
+        value: file2[key],
+      });
     }
   });
 
   const rr = `
 {
 ${resultArr
-  .map((elem) => `  ${elem.type} ${elem.key}: ${elem.value}`)
-  .join("\n")}
+    .map((elem) => `  ${elem.type} ${elem.key}: ${elem.value}`)
+    .join('\n')}
 }
 `;
   return rr;
 };
+
+export default diff;
