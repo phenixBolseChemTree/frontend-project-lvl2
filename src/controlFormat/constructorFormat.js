5763cd3a591e35b08767b90f803297600175a3f3
ivan sharapov <ivansharapov@ivans-MacBook-Pro.local>
2023-05-26 18:55:09 +0200

    step 8 format JSON (in constructor)

--- src/controlFormat/constructorFormat.js
+++ src/controlFormat/constructorFormat.js
@@ -4,6 +4,8 @@ export default (nodeTree, format) => {
   switch (format) {
     case 'stylish':
       return stylish(nodeTree);
+    case 'json':
+      return JSON.stringify(nodeTree);
     default:
       throw new Error(`Uncnown ${format}`);
   }
